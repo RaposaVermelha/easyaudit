344a4851e12e16026139791d4fe4ca47a4335584
RaposaVermelha <matheus.adm7102@gmail.com>
2026-04-06 20:51:28 -0400

    Project Created

---
+++ src/main/java/com/easyaudit/easyaudit/EasyauditApplication.java
@@ -0,0 +1,13 @@
+package com.easyaudit.easyaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyauditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyauditApplication.class, args);
+	}
+
+}
