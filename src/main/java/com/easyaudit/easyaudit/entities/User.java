1d03a9707cb44aedcddc2863ee25dbe0f285aaf6
RaposaVermelha <matheus.adm7102@gmail.com>
2026-04-07 00:11:16 -0400

    Users entity and resources

---
+++ src/main/java/com/easyaudit/easyaudit/entities/User.java
@@ -0,0 +1,82 @@
+package com.easyaudit.easyaudit.entities;
+
+import java.util.List;
+import java.util.Objects;
+
+public class User {
+	
+	private Long id;
+	private String name;
+	private String password;
+	private String email;
+	private List<String> role;
+	
+	public User(Long id, String name, String password, String email, List<String> role) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<String> getRole() {
+		return role;
+	}
+
+	public void setRole(List<String> role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/easyaudit/easyaudit/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.easyaudit.easyaudit.resources;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.easyaudit.easyaudit.entities.User;
+
+@RestController
+@RequestMapping(value = "/users") //teste de rota
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Raposo", "123", "raposo@gmail.com", List.of("ADMIN"));
+		return ResponseEntity.ok().body(u); // retorna o usuario no body
+	}
+}
