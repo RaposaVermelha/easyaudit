165524bc51cf51dc6884e6650907ca544a422183
RaposaVermelha <matheus.adm7102@gmail.com>
2026-04-14 22:42:30 -0400

    Create Database Test

--- src/main/java/com/easyaudit/easyaudit/entities/User.java
+++ src/main/java/com/easyaudit/easyaudit/entities/User.java
@@ -1,15 +1,35 @@
 package com.easyaudit.easyaudit.entities;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 
-public class User {
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String password;
 	private String email;
+	
+	@ElementCollection
 	private List<String> role;
+	
+	public User() {
+	}
 	
 	public User(Long id, String name, String password, String email, List<String> role) {
 		super();
